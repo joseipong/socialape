e13ff7ecfc7fa0b48c6c55287f986dd0ee8029b8
Jose Martin Ipong <jose.ipong@awsys-i.com>
2020-07-07 18:38:48 +0800

    Added updates for day 2 tutorials Image upload Add and get user profile details Getting and commenting of post

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+    users: [
+        {
+            userId: 'xO3mLEH0MpeeL4ijpf3s4ask8po2',
+            email: 'user@email.com',
+            handle: 'user',
+            createdAt: '2020-07-06T10:22:16.525Z',
+            imageUrl: 'image/lsdkflasdlkfas/adsfdasf',
+            bio: 'Hello, my name is user, nice to meet you',
+            website: 'https://user.com',
+            location: 'London, UK'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
@@ -7,5 +19,37 @@ let db = {
             likeCount: 5,
             commentCount: 2
         }
-    ]
+    ],
+    comments: [
+        {
+          userHandle: 'user',
+          screamId: 'kdjsfgdksuufhgkdsufky',
+          body: 'nice one mate!',
+          createdAt: '2019-03-15T10:59:52.798Z'
+        }
+    ],
 }
+
+const userDetails = {
+    // Redux data
+    credentials: {
+      userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'https://user.com',
+      location: 'Lonodn, UK'
+    },
+    likes: [
+      {
+        userHandle: 'user',
+        screamId: 'hh7O5oWfWucVzGbHH2pa'
+      },
+      {
+        userHandle: 'user',
+        screamId: '3IOnFoQexRcofs5OhBXO'
+      }
+    ]
+  };
--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,20 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
-const { defaultDatabase } = require('firebase-functions/lib/providers/firestore');
-
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/users');
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 // Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // Users routes
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region('asia-east2').https.onRequest(app);
